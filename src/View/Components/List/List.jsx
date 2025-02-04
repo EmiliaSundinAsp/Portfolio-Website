8bde2e813500541c44cd2c5247bede6b4d1e4acc
EmiliaSundinAsp <emiliasundinasp@gmail.com>
2025-02-04 11:33:04 +0100

    Updated info on website

--- src/View/Components/List/List.jsx
+++ src/View/Components/List/List.jsx
@@ -1,6 +1,6 @@
 import './List.css';
 
-function List({heading, listItem, listItem2, listItem3, listItem4, listItem5, listItem6}) {
+function List({heading, listItem, listItem2, listItem3, listItem4, listItem5, listItem6, listItem7, listItem8}) {
 	return (
 		<div className='List'>
 			<ul>
@@ -10,7 +10,9 @@ function List({heading, listItem, listItem2, listItem3, listItem4, listItem5, li
 				<li className='ListItem'>{listItem3}</li>
 				<li className='ListItem'>{listItem4}</li>
 				<li className='ListItem'>{listItem5}</li>
-				<li className='ListItem'>{listItem6}</li>
+        <li className='ListItem'>{listItem6}</li>
+        <li className='ListItem'>{listItem7}</li>
+				<li className='ListItem'>{listItem8}</li>
 			</ul>
 		</div>
 	);
--- src/View/Components/PageContainer.jsx
+++ src/View/Components/PageContainer.jsx
@@ -43,17 +43,17 @@ function PageContainer () {
 					<Route path="/about" element={<MainPage>
 						<Title title="About"/>
 						<div className='AboutContainer'>
-							<Section heading="Who's Emilia Sundin Asp?" paragraph="I am a 35 year old woman living i Norrköping with my boyfriend of many years, and our awesome 4-year-old boy. I consider myself creative and like painting, interior design and web design (although I am a complete novis at the latter!)."/>
+							<Section heading="Who's Emilia Sundin Asp?" paragraph="I am a 36 year old woman living i Norrköping with my boyfriend of many years, and our awesome 5-year-old boy. I consider myself creative and like painting, interior design and web design (although I am a complete novis at the latter!)."/>
 							<Section heading="Was Emilia really a teacher?!" paragraph="Yes, indeed. I started my teacher's education in 2010, and worked as a teacher until early 2023. I then decided it was time to try my hand at something new, and started my journey towards becoming a front end developer."/>
 							<Section heading="Why did Emilia change career?" paragraph="Being a teacher has always been a dream of mine, and when it was time to choose a career, it seemed the only possible choice. But after working as a teacher for 8 years, I started yearning to explore something new."/>
-							<Section heading="What is Emilia studying?" paragraph="I'm studying my first year at KYH Anywhere to become a front end developer. So far I've learned a lot, but it only makes me realize how expansive the field of developing is. But no matter, I'm determined to learn as much as possible!"/>
+							<Section heading="What is Emilia studying?" paragraph="I'm studying my second year at KYH Anywhere to become a front end developer. I'm currently doing my internship at a company in Linköping until April. After that I'll do a final project and finish my education in May."/>
 						</div>
 					</MainPage>} />
 					<Route path="/skills" element={<MainPage>
 						<Title title="Skills"/>
 						<div className='SkillsContainer'>
-							<List heading="General skills:" listItem="Attention to detail" listItem2="Creative - with an eye for design" listItem3="Good team player" listItem4="Persistant" listItem5="Self reliant" listItem6="Fantastic sense of humor"/>
-							<List heading="Coding skills:" listItem="Vanilla JavaScript" listItem2="Semantic HTML" listItem3="CSS/SASS" listItem4="React" listItem5="... and working on more!"/>
+							<List heading="General skills:" listItem="Attention to detail" listItem2="Creative - with an eye for design" listItem3="Good team player" listItem4="Persistant" listItem5="Self reliant" listItem6="Good language and communication" listItem7="Eager to learn" listItem8="A fantastic sense of humor (if you like puns...)"/>
+							<List heading="Coding skills:" listItem="JavaScript" listItem2="HTML" listItem3="CSS" listItem4="SASS" listItem5="Bootstrap" listItem6="TypeScript" listItem7="Figma" listItem8="... and working on more!"/>
 						</div>
 					</MainPage>} />
 					<Route path="/projects" element={<MainPage>
